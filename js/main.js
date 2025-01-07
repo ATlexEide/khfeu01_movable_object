e114aef47ca5b19d7d2ca002730e849ea65b46b6
Alex <atlexeide@gmail.com>
2025-01-07 09:37:12 +0100

    Add eventlistener to map

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 const map = document.getElementById("map");
 const player = document.getElementById("player");
+
+map.addEventListener("click", (e) => {
+  console.log(e.clientX, e.clientY);
+  //   player.style.scale = 0.5
+  player.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+});
