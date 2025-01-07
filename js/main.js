5e406dd513229ffc1886ac946a3c4756b924ac93
Alex <atlexeide@gmail.com>
2025-01-07 09:40:24 +0100

    Remove unused code

--- js/main.js
+++ js/main.js
@@ -4,6 +4,5 @@ const player = document.getElementById("player");
 // Move object to clicked coordinates on screen
 map.addEventListener("click", (e) => {
   console.log(e.clientX, e.clientY);
-  //   player.style.scale = 0.5
   player.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
 });
