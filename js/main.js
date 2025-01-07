aec2fa622609072b334e9ae272564280912c1f20
Alex <atlexeide@gmail.com>
2025-01-07 10:22:07 +0100

    Add base movement logic

--- js/main.js
+++ js/main.js
@@ -6,3 +6,35 @@ map.addEventListener("click", (e) => {
   console.log(e.clientX, e.clientY);
   player.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
 });
+//
+let posX = 0;
+let posY = 0;
+let step = 50;
+document.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "ArrowUp":
+      posY -= step;
+      console.log(posY);
+      player.style.transform = `translate(${posX}px, ${posY}px)`;
+      break;
+    case "ArrowDown":
+      posY += step;
+      console.log(posY);
+      player.style.transform = `translate(${posX}px, ${posY}px)`;
+
+      break;
+    case "ArrowLeft":
+      posX -= step;
+      console.log(posX);
+      player.style.transform = `translate(${posX}px, ${posY}px)`;
+      break;
+    case "ArrowRight":
+      posX += step;
+      console.log(posX);
+      player.style.transform = `translate(${posX}px, ${posY}px)`;
+      break;
+
+    default:
+      break;
+  }
+});
