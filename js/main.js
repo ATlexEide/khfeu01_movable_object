7160bd0ae2b2fd726b23ab7f3b976f23e423da2d
Alex <atlexeide@gmail.com>
2025-01-07 09:39:59 +0100

    Add comment

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const map = document.getElementById("map");
 const player = document.getElementById("player");
 
+// Move object to clicked coordinates on screen
 map.addEventListener("click", (e) => {
   console.log(e.clientX, e.clientY);
   //   player.style.scale = 0.5
