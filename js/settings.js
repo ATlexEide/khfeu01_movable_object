19f39141597176886a16123210d140a2797fdaae
Alex <atlexeide@gmail.com>
2025-01-08 12:34:50 +0100

    Fix logic

--- js/settings.js
+++ js/settings.js
@@ -8,35 +8,36 @@ export class Settings {
   stepSizeInput = document.getElementById("step-size");
   // Dialog
   settingsDialog = document.getElementById("settings-dialog");
-  constructor(playerSize, stepSize) {
+  constructor(playerSize = 100, stepSize = 50) {
     this.stepSize = stepSize;
     this.playerSize = playerSize;
   }
-  init = () => {
+  init = (map, player) => {
     // Open settings dialog
     this.settingsBtn.addEventListener("click", () => {
-      settingsDialog.showModal();
+      this.settingsDialog.showModal();
     });
 
     // Cancel settings and close dialog
     this.cancelSettingsBtn.addEventListener("click", (e) => {
       e.preventDefault();
       console.log("yippie");
-      settingsDialog.close();
+      this.settingsDialog.close();
     });
     // Apply settings
     this.applySettingsBtn.addEventListener("click", (e) => {
       e.preventDefault();
-      console.log("player size: ", playerSizeInput.value);
-      console.log("step size: ", stepSizeInput.value);
+      console.log("player size: ", this.playerSizeInput.value);
+      console.log("step size: ", this.stepSizeInput.value);
       console.log("yippie");
-      changePlayerSize(playerSizeInput.value);
-      changeStepSize(stepSizeInput.value);
-      updateBorder();
-      settingsDialog.close();
+      this.changePlayerSize(player, this.playerSizeInput.value);
+      this.changeStepSize(player, this.stepSizeInput.value);
+      map.updateBorder(this);
+      this.settingsDialog.close();
     });
   };
   changePlayerSize(player, size) {
+    this.playerSize = size;
     player.playerSize = size;
     player.object.style.width = `${player.playerSize}px`;
   }
