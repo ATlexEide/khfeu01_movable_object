409fd74148a74f8047129cbe707da48b44ee46bd
Alex <atlexeide@gmail.com>
2025-01-08 10:57:35 +0100

    Move player related options to own file

---
+++ js/player.js
@@ -0,0 +1,45 @@
+export class Player {
+  constructor(settings, map) {
+    (this.object = document.getElementById("player")),
+      (this.playerSize = settings.playerSize),
+      (this.stepSize = settings.stepSize),
+      (this.position = {
+        x: map.border.x / 2 - settings.playerSize,
+        y: map.border.y / 2 - settings.playerSize,
+      });
+  }
+  updatePos(x, y) {
+    this.position.x = x;
+    if (this.position.x < 0) this.position.x = 0;
+    if (this.position.x > properties.map.border.x)
+      this.position.x = properties.map.border.x;
+
+    this.position.y = y;
+    if (this.position.y < 0) this.position.y = 0;
+    if (this.position.y > properties.map.border.y)
+      this.position.y = properties.map.border.y;
+    player.style.transform = `translate(${this.position.x}px, ${this.position.y}px)`;
+  }
+  resetPos() {
+    this.position.x = properties.map.object.offsetWidth / 2 - this.playerSize;
+    this.position.y = properties.map.object.offsetHeight / 2 - this.playerSize;
+  }
+  move(key) {
+    switch (key) {
+      case "ArrowRight":
+        this.position.x += this.stepSize;
+        break;
+      case "ArrowUp":
+        this.position.y -= this.stepSize;
+        break;
+      case "ArrowDown":
+        this.position.y += this.stepSize;
+        break;
+      case "ArrowLeft":
+        this.position.x -= this.stepSize;
+        break;
+    }
+    console.log(`X: ${this.position.x}, Y: ${this.position.y}`);
+    updatePos(this.position.x, this.position.y);
+  }
+}
