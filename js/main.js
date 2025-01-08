7b34ad51a1ca992f55d3fc3e73977e0eadc971df
Alex <atlexeide@gmail.com>
2025-01-08 12:33:47 +0100

    Move eventlisteners

--- js/main.js
+++ js/main.js
@@ -4,41 +4,5 @@ import { Settings } from "./settings.js";
 const settings = new Settings(100, 50);
 const map = new Map(settings);
 const player = new Player(settings, map);
-console.log(settings);
-//// Settings
-settings.init();
-// Player options
-settings.changePlayerSize(player, 100);
-
-// Move object to clicked coordinates on screen
-let isMouseClicking = false;
-map.addEventListener("mousemove", (e) => {
-  if (isMouseClicking)
-    updatePos(
-      e.clientX - properties.player.playerSize / 2,
-      e.clientY - properties.player.playerSize / 2
-    );
-});
-map.addEventListener("mousedown", (e) => {
-  updatePos(
-    e.clientX - properties.player.playerSize / 2,
-    e.clientY - properties.player.playerSize / 2
-  );
-  console.log(isMouseClicking);
-  isMouseClicking = true;
-});
-map.addEventListener("mouseup", () => {
-  isMouseClicking = false;
-});
-//
-document.addEventListener("keydown", (e) => {
-  move(e.key);
-});
-
-function updateBorder() {
-  properties.map.border.x =
-    properties.map.object.offsetWidth - properties.player.playerSize;
-  properties.map.border.y =
-    properties.map.object.offsetHeight - properties.player.playerSize;
-}
-properties.init();
+settings.init(map, player);
+map.init(settings, player);
