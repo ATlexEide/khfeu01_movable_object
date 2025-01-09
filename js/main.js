9f8ddbfa396990d5ec5c0d0d0154f683f633fab7
Alex <atlexeide@gmail.com>
2025-01-09 09:14:22 +0100

    .

--- js/main.js
+++ js/main.js
@@ -8,8 +8,7 @@ const player = new Player(settings, map);
 settings.init(map, player);
 map.init(settings, player);
 // Place an obstacle (x axis, y axis, optional size in px)
-new Obstacle(200, 200, 100).place();
-new Obstacle(600, 200).place();
+
 new Obstacle(420, 451, 300).place();
-new Obstacle(1020, 51, 130).place();
-new Obstacle(1020, 511, 130).place();
+// new Obstacle(1020, 51, 130).place();
+// new Obstacle(1020, 511, 130).place();
