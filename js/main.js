c490b28837f0cc468fa011db83873aad0ec62c63
Alex <atlexeide@gmail.com>
2025-01-13 19:26:44 +0100

    .

--- js/main.js
+++ js/main.js
@@ -7,13 +7,10 @@ export const map = new Map(settings);
 const player = new Player(settings, map);
 settings.init(map, player);
 map.init(settings, player);
-// Place an obstacle (x axis, y axis, optional size in px)
 
-// new Obstacle(420, 451, 300);
-// size must be 100 or more
+// Place an obstacle (x axis, y axis, optional size in px)
 new Obstacle(720, 381, 130);
-// new Obstacle(1020, 511, 130);
-new Obstacle(0, 500, 100);
+new Obstacle(0, 500);
 
 console.log("player size", player.playerSize);
 console.log("player size | settings", settings.playerSize);
